689de68e20bad02fbb5846d22e4848749cd5417f
Manojkumar886 <maddymano434@gmail.com>
2023-07-28 15:02:15 +0530

    function and fat arrow is finished

---
+++ JAVA SCRIPT/UsingFunction.js
@@ -0,0 +1,25 @@
+function func1()
+{
+    alert("welcome to java script method or called as function");
+}
+
+function funcwithargu2(username,mailid)
+{
+    alert("my github user name is "+username+"\n my github mail id is "+mailid)
+}
+function funcwithargu3(x,y)
+{
+    return alert('my x value is '+x+"\my y value is "+y+"\n x/y value is"+x / y);
+}
+
+// fat arrow function( =()=> )
+
+let fatarrow=()=>
+{
+    console.log(11+89);
+}
+
+var fatarrow2=(a,b)=>
+{
+    return a/b;
+}
