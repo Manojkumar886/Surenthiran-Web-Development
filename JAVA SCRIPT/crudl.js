847230532ae1fd3f4853b07ede3c771aff469ad5
Manojkumar886 <maddymano434@gmail.com>
2023-08-03 14:22:25 +0530

    array crudl is complted

--- JAVA SCRIPT/crudl.js
+++ JAVA SCRIPT/crudl.js
@@ -19,3 +19,46 @@ let viewing=()=>
     document.getElementById('listall').innerHTML="<table><thead><tr><th>FlowersNames</th></tr></thead><tbody>"+all+"</tbody></table>"
     
 }
+
+let rearranging=()=>
+{
+  const index=document.getElementById("uservalue").value ;
+  
+  let newvalue=prompt("please telll your new flower name"+flowersname[index]);
+
+  flowersname[index]=newvalue;
+  alert(newvalue+" has been updated successfully")
+  viewing()
+}
+
+
+let erasing=()=>
+{
+  let deletedvalue=document.getElementById("uservalue").value ;
+
+
+  flowersname=flowersname.filter((data)=>
+  {
+    return data!==deletedvalue;
+  })
+  viewing()
+}
+
+const checking=()=>
+{
+  let checking=document.getElementById("uservalue").value ;
+  
+  for(let pos=0;pos<flowersname.length;pos++)
+  {
+    if(flowersname[pos]===checking)
+    {
+      return pos;
+    }
+  }
+}
+
+let sorting=()=>
+{
+  flowersname.sort();
+  viewing()
+}
